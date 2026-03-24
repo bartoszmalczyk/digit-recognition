75566c3ecb422e41f8af5d7d10db8759cc05c8af
Bartosz Malczyk <bartoszmalczyk@icloud.com>
2026-03-24 19:58:40 +0100

    accepting user input

---
+++ predict.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageOps
+import numpy as np
+import tensorflow as tf 
+
+img = Image.open('your_digit.png').convert('L') 
+img = img.resize((28,28))
+
+img_array = np.array(img)
+img_array = img_array.astype('float32') / 255.0
+img_array = img_array.reshape(1, 784)
+
+model = tf.keras.models.load_model('digit_recognition.keras')
+
+prediction = model.predict(img_array)
+wynik = np.argmax(prediction)
+
+print(f"Detected number: {wynik}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+img_array = img_array.astype('float32') / 255.0
+
+# model = tf.keras.models.load_model('digit_recognition.keras')
+# prediction = model.predict(img)
+# wynik = np.argmax(prediction)
+# print(wynik)
